708022c5f2e7a7a57550c9cdaa019d7ecfb07587
Simpleshaikh1 <Hamzatbolaji648@yahoo.com>
2023-07-06 11:23:59 +0100

    switch badges for mission

--- src/components/MissionButton.js
+++ src/components/MissionButton.js
@@ -16,7 +16,7 @@ const MissionButton = ({ id, reserved }) => {
       className={`${
         reserved
           ? 'text-blue-500 bg-transparent cursor-pointer mt-3 rounded-full px-[1.2rem] py-2 font-medium outline outline-red-500'
-          : 'text-gray-500 bg-transparent cursor-pointer mt-3 rounded px-[1.2rem] py-2 font-medium outline outline-gray-500'
+          : 'text-gray-500 bg-transparent cursor-pointer mt-3 rounded px-[1.2rem] py-2 font-medium outline outline-gray-400'
       }`}
       onClick={() => click(id)}
     >
