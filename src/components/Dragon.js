e951d5acad7aa7aa25d9d6ef330b84b701fb203f
hiwina <hiwina@gmail.com>
2023-07-06 20:02:21 +0300

    fetch data

--- src/components/Dragon.js
+++ src/components/Dragon.js
@@ -1,9 +1,15 @@
-function Dragon() {
+const Dragon = (dragons) => {
+  const { name, img, desc } = dragons;
   return (
-    <div>
-      <h2>Dragon Page</h2>
+    <div className=" d-flex-space g-10 my-20">
+      <img src={img} className="logo" alt="dragon" />
+      <div className="dragon-details">
+        <h2 className="dragon-name">{name}</h2>
+        <p className="dragon-desc">{desc}</p>
+        <button type="button" className="dragon-reserve btn">Reserve Dragon</button>
+      </div>
     </div>
   );
-}
+};
 
 export default Dragon;
---
+++ src/pages/DragonsPage.js
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchDragons } from '../redux/dragons/dragonSlice';
+import Dragon from '../components/Dragon';
+
+const Dragons = () => {
+  const dragons = useSelector((store) => store.dragons);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchDragons());
+  }, [dispatch]);
+  return (
+    <div className="container">
+      {dragons.loading && <div>loading</div>}
+      {!dragons.loading && dragons.error ? (
+        <div>
+          Error
+          {dragons.error}
+        </div>
+      ) : null}
+      {!dragons.loading && dragons.rocket.length ? (
+        dragons.rocket.map((dragon) => (
+          <Dragon
+            key={dragon.id}
+            name={dragon.name}
+            img={dragon.flickr_images}
+            id={dragon.id}
+            desc={dragon.description}
+          />
+        ))
+      ) : null}
+    </div>
+  );
+};
+
+export default Dragons;
